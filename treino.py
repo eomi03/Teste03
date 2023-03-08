9f18f015ec6540ff9dfbd122a176cb15e3c774e1
eomi03 <miguel.cirico03@gmail.com>
2023-03-07 22:07:56 -0300

    Create treino.py

---
+++ treino.py
@@ -0,0 +1,14 @@
+print('CALCULADORA')
+num1 = int(input('informe o primeiro numero:'))
+
+num2 = int(input('informe o segundo numero:'))
+resultado = num1 * num2 
+print(resultado)
+
+#somar: +
+#subtrair: -
+#multiplicar: *
+#dividir: /
+
+
+
