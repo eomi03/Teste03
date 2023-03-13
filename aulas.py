78065943aaa4b97624e155bf366f059edac86126
eomi03 <126923813+eomi03@users.noreply.github.com>
2023-03-13 16:41:35 -0300

    Delete aulas.py

--- aulas.py
+++
@@ -1,7 +0,0 @@
-days = int(input("insira a quantidade de dias:"))
-hr = int(input("insira a quantidade de horas:"))
-min = int(input("insira a quantidade de minutos:"))
-seg = int(input("insira a quantidade:"))
-
-segtotal = days*86400+hr*3600+min*60+seg
-print(f"o total de segundos é,{segtotal}")
